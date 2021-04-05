cc54602b902dafeb69ba7e7a3cf87da516ef5c90
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2021-04-05 15:27:36 +0300

    Blinov "Java From Epam" first commit

---
+++ src/by/epam/learn/action/TutorialAction.java
@@ -0,0 +1,9 @@
+package by.epam.learn.action;
+
+public class TutorialAction {
+
+    public void printMessage(String msg) {
+        //output string
+        System.out.println(msg);
+    }
+}
---
+++ src/by/epam/learn/main/HelloTutorial.java
@@ -0,0 +1,7 @@
+package by.epam.learn.main;
+
+public class HelloTutorial {
+    public static void main(String[] args) {
+        System.out.println("tutorial -> https://docs.oracle.com/javase/tutorial/");
+    }
+}
